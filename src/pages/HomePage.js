501303a55885e88efa9fc48738186a4aae4cd847
vasuldutschak <vasuldutschak@mail.ru>
2024-02-28 15:31:43 +0100

    add link and page for pokemons

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,18 @@
+import Loader from "../components/loader/Loader";
+import {Show} from "../components/show/Show";
 
 function HomePage(props) {
     return (
-        <h1>Home Page</h1>
+       <>
+         <Show>
+             <Show.When isTrue={true}>
+                 <h1>Home Page</h1>
+             </Show.When>
+             <Show.Else>
+                 <h1>HOME PAGE</h1>
+             </Show.Else>
+         </Show>
+       </>
     );
 }
 
---
+++ src/pages/PokemonsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PokemonsPage(props) {
+    return (
+        <>
+            <h1>POKEMONS PAGE</h1>
+        </>
+    );
+}
+
+export default PokemonsPage;
