03cbc9a4dd68ddf88083c0426c16c3c212a043b8
vasuldutschak <vasuldutschak@mail.ru>
2024-02-28 15:30:35 +0100

    create loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SuspenseLoaderStyled} from "./Loader.styled";
+
+function Loader(props) {
+    return (
+        <SuspenseLoaderStyled></SuspenseLoaderStyled>
+    );
+}
+
+export default Loader;
